f8bac1e274a281badc6a053935d519b02c1ad25a
Josh Holmes <josh@glowingmanagement.com>
2022-09-10 20:05:50 +0100

    link stripe

--- src/StripeApp.js
+++ src/StripeApp.js
@@ -1,8 +1,34 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import { View, Text, StyleSheet, TextInput, Button, Alert } from "react-native";
+import { CardField, useConfirmPayment } from "@stripe/stripe-react-native";
 
 const StripeApp = () => {
   const [email, setEmail] = useState();
+  const [cardDetails, setCardDetails] = useState();
+  const { confirmPayment, loading } = useConfirmPayment();
+
+  const API_URL = "https://localhost:3000";
+
+  const fetchPaymentIntentClientSecret = async () => {
+    const response = await fetch(`${API_URL}/create-payment-intent`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
+
+  const handlePayPress = async () => {
+    if (!cardDetails?.complete || !email) {
+      Alert.alert("Please enter complete card details and email");
+      return;
+    }
+
+    const billingDetails = {
+      email: email,
+    };
+  };
+
   return (
     <View style={styles.container}>
       <TextInput
@@ -12,6 +38,16 @@ const StripeApp = () => {
         onChange={(value) => setEmail(value.nativeEvent.text)}
         style={styles.input}
       />
+      <CardField
+        postalCodeEnabled={true}
+        placeholder={{ number: "4242 4242 4242 4242" }}
+        cardStyle={styles.card}
+        style={styles.cardConatiner}
+        onCardChange={(cardDetails) => {
+          setCardDetails(cardDetails);
+        }}
+      />
+      <Button onPress={handlePayPress} title="Pay" disabled={loading} />
     </View>
   );
 };
@@ -31,5 +67,13 @@ const styles = StyleSheet.create({
     fontSize: 20,
     height: 50,
     padding: 10,
+  },
+  card: {
+    backgroundColor: "#efefefef",
+  },
+
+  cardConatiner: {
+    height: 50,
+    marginVertical: 30,
   },
 });
